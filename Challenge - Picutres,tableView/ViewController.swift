047ac3e7b48f966fc885f164b44c248a698eedbe
igniti0n <ivanat735@gmail.com>
2021-08-09 08:48:04 +0200

    Initial Commit

---
+++ Challenge - Picutres,tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge - Picutres,tableView
+//
+//  Created by Ivan Stajcer on 09.08.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
